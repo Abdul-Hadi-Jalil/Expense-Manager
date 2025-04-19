cf0f982b2d84d2a41e150da039c26d0c39762652
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-04-19 22:20:05 +0500

    Completed the UI of the home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Expense Tracker"),
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+          foregroundColor: Colors.white,
+          bottom: TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            indicatorColor: Colors.white,
+            tabs: [Tab(text: "By Date"), Tab(text: 'By Category')],
+          ),
+        ),
+        drawer: Drawer(),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Date Content')),
+            Center(child: Text('Category Content')),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.cyan[300],
+          child: Icon(Icons.add, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
